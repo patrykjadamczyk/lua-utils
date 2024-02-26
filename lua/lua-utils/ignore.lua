c60ef4d6c36a68a49cf08e643b1858d638830175
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-02-26 22:49:17 +0000

    Changed doc comment for ignore module

--- lua/lua-utils/ignore.lua
+++ lua/lua-utils/ignore.lua
@@ -1,28 +1,28 @@
 local M = {}
 
 ---Ignore package imported and not used
----@return nil
+---@type fun(...: any): nil
 -- luacheck: ignore 212
 -- selene: allow(unused_variable)
 M.IKnowThatThisPackageIsNotUsed = function (...)
 end
 
 ---Ignore variable declared and not used
----@return nil
+---@type fun(...: any): nil
 -- luacheck: ignore 212
 -- selene: allow(unused_variable)
 M.IKnowThatThisVariableIsNotUsed = function (...)
 end
 
 ---Ignore package imported and not used for debugging packages
----@return nil
+---@type fun(...: any): nil
 -- luacheck: ignore 212
 -- selene: allow(unused_variable)
 M.PackagesUsedInDebugging = function (...)
 end
 
 ---Ignore package imported and not used
----@return nil
+---@type fun(...: any): nil
 -- luacheck: ignore 212
 -- selene: allow(unused_variable)
 M.PossiblyUsingThis = function (...)
