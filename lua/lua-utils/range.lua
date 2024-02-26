0ad0178287a0763894c2440cb1c88188639cd86c
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-02-26 22:21:33 +0000

    Changed logic to be less copy pasty in range module

--- lua/lua-utils/range.lua
+++ lua/lua-utils/range.lua
@@ -1,61 +1,5 @@
 local M = {}
 
----Range function returns an array to be used with for i in range.Range(end)
----This index will start from 0 to end
----@type fun(stop: number): number[]
-M.Range = function(stop)
-  assert(( type(stop) == type(0) ), "stop must be a number")
-  local returns = {}
-  local i = 0
-  while i <= stop do
-    returns[#returns+1] = i
-    i = i + 1
-  end
-  return returns
-end
-
----Range function returns an array to be used with for i in range.RangeNonInclusive(end)
----This index will start from 0 to end - 1
----@type fun(stop: number): number[]
-M.RangeNonInclusive = function(stop)
-  assert(( type(stop) == type(0) ), "stop must be a number")
-  local returns = {}
-  local i = 0
-  while i < stop do
-    returns[#returns+1] = i
-    i = i + 1
-  end
-  return returns
-end
-
----Range function returns an array to be used with for i in range.StartEndRange(start, end)
----@type fun(start: number, stop: number): number[]
-M.StartEndRange = function(start, stop)
-  assert(( type(start) == type(0) ), "start must be a number")
-  assert(( type(stop) == type(0) ), "stop must be a number")
-  local returns = {}
-  local i = start
-  while i <= stop do
-    returns[#returns+1] = i
-    i = i + 1
-  end
-  return returns
-end
-
----Range function returns an array to be used with for i in range.StartEndNonInclusiveRange(start, end)
----@type fun(start: number, stop: number): number[]
-M.StartEndNonInclusiveRange = function(start, stop)
-  assert(( type(start) == type(0) ), "start must be a number")
-  assert(( type(stop) == type(0) ), "stop must be a number")
-  local returns = {}
-  local i = start
-  while i < stop do
-    returns[#returns+1] = i
-    i = i + 1
-  end
-  return returns
-end
-
 ---Range function returns an array to be used with for i in range.ComplexRange(start, end, step)
 ---@type fun(start: number, stop: number, step: number): number[]
 M.ComplexRange = function(start, stop, step)
@@ -84,6 +28,32 @@ M.ComplexNonInclusiveRange = function(start, stop, step)
     i = i + step
   end
   return returns
+end
+
+---Range function returns an array to be used with for i in range.Range(end)
+---This index will start from 0 to end
+---@type fun(stop: number): number[]
+M.Range = function(stop)
+  return M.ComplexRange(0, stop, 1)
+end
+
+---Range function returns an array to be used with for i in range.RangeNonInclusive(end)
+---This index will start from 0 to end - 1
+---@type fun(stop: number): number[]
+M.RangeNonInclusive = function(stop)
+  return M.ComplexNonInclusiveRange(0, stop, 1)
+end
+
+---Range function returns an array to be used with for i in range.StartEndRange(start, end)
+---@type fun(start: number, stop: number): number[]
+M.StartEndRange = function(start, stop)
+  return M.ComplexRange(start, stop, 1)
+end
+
+---Range function returns an array to be used with for i in range.StartEndNonInclusiveRange(start, end)
+---@type fun(start: number, stop: number): number[]
+M.StartEndNonInclusiveRange = function(start, stop)
+  return M.ComplexNonInclusiveRange(start, stop, 1)
 end
 
 return M
